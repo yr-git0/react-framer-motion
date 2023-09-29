a79a0daa4bdc488a4d1411decf9821cc1bd9b860
Yaerin Park <programmer94@naver.com>
2023-09-29 23:55:56 +0900

    SVG Animation

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {
 import styled from "styled-components";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -29,6 +29,25 @@ const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
   drag: { backgroundColor: "rgb(254, 202, 87)", transition: { duration: 3 } },
+};
+
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  color: transparent;
+  path {
+    stroke: white;
+    stroke-width: "2";
+  }
+`;
+
+const svgVariant = {
+  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+  end: {
+    pathLength: 1,
+    fill: "rgba(255, 255, 255, 1)",
+    transition: { duration: 3 },
+  },
 };
 
 function App() {
@@ -52,7 +71,18 @@ function App() {
 
   return (
     <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, rotate, scale }} drag="x" dragSnapToOrigin />
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="1em"
+        viewBox="0 0 448 512"
+      >
+        <motion.path
+          variants={svgVariant}
+          initial={"start"}
+          animate={"end"}
+          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        />
+      </Svg>
     </Wrapper>
   );
 }
