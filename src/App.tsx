05f2bf76cbbbcdff7e507cd4bdc273e74a9711b0
Yaerin Park <programmer94@naver.com>
2023-10-01 01:59:23 +0900

    make Slider using AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useScroll,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled(motion.div)`
@@ -14,78 +8,75 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
+  position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(254, 202, 87)", transition: { duration: 3 } },
-};
-
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  color: transparent;
-  path {
-    stroke: white;
-    stroke-width: "2";
-  }
-`;
-
-const svgVariant = {
-  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
-  end: {
-    pathLength: 1,
-    fill: "rgba(255, 255, 255, 1)",
-  },
+  initial: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  visible: (isBack: boolean) => ({
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  }),
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 1,
+    },
+  }),
 };
 
 function App() {
-  const x = useMotionValue(0);
-  const rotate = useTransform(x, [-300, 300], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-300, 0, 300],
-    [
-      "linear-gradient(135deg,#1bebfa,#056eaa)",
-      "linear-gradient(135deg,#e09,#d0e)",
-      "linear-gradient(135deg,#ffe608,#918701)",
-    ]
-  );
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
-  // useMotionValueEvent(scrollY, "change", (latest) => {
-  //   console.log(latest);
-  // });
+  const [visible, setVisible] = useState(1);
+  const [isBack, setIsBack] = useState(false);
+  const prevPlease = () => {
+    setIsBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  const nextPlease = () => {
+    setIsBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
 
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="1em"
-        viewBox="0 0 448 512"
-      >
-        <motion.path
-          variants={svgVariant}
-          initial={"start"}
-          animate={"end"}
-          transition={{
-            default: { duration: 5 },
-            fill: { duration: 2, delay: 5 },
-          }}
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        />
-      </Svg>
+    <Wrapper>
+      <AnimatePresence custom={isBack}>
+        <Box
+          custom={isBack}
+          key={visible}
+          variants={boxVariants}
+          initial="initial"
+          animate="visible"
+          exit="exit"
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
